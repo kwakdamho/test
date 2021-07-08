3073c0dc8b5039fc376e804db78fe43115a4950a
wkdru <wkdrudgns369@daum.net>
2021-07-08 17:44:22 +0900

    7/8 temporary class commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 def hello_world(request):
     if request.method == 'POST':
         return render(request, 'accountapp/hello_world.html',
-                      context={'text':'POST METHOD!'})
-    else: return render(request, 'accountapp/hello_world.html',
-                      context={'text':'GET METHOD!'})
+                      context={'text': 'POST METHOD!'})
+    else:
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text': 'GET METHOD!'})
