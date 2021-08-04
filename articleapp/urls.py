afc9106088f1cf40bcc117e237a00adc725135dc
wkdru <wkdrudgns369@daum.net>
2021-08-04 11:04:27 +0900

    bugfix: 8/3 temporary class commit 1

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -7,6 +7,5 @@ app_name = 'articleapp'
 
 urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
-    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
     path('create/', ArticleCreateView.as_view(), name='create'),
 ]
