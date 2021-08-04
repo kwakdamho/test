90d764485b776c30c8f48322790232572969f326
wkdru <wkdrudgns369@daum.net>
2021-08-04 12:12:34 +0900

    8/4 temporary class commit 4

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
@@ -31,8 +31,14 @@ class ArticleUpdateView(UpdateView):
     model = Article
     form_class = ArticleCreationForm
     context_object_name = 'target_article'
-    # success_url = reverse_lazy('articleapp:')
     template_name = 'articleapp/update.html'
 
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    context_object_name = 'target_article'
+    success_url = reverse_lazy('articleapp:list')
+    template_name = 'articleapp/delete.html'
